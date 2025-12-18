f257bb9131952edf2ff6bd3838a1de4e4751fdb0
Floraluke <kiby1025@qq.com>
2025-12-18 23:55:10 +0800

    实验三.QtSQL数据库模块与View-Model结构的使用

---
+++ Lab4/idatabase.h
@@ -0,0 +1,52 @@
+#ifndef IDATABASE_H
+#define IDATABASE_H
+
+#include <QObject>
+#include <QtSql>
+#include <QSqlDatabase>
+#include <QDataWidgetMapper>
+#include <QItemSelectionModel>
+
+class IDatabase : public QObject
+{
+    Q_OBJECT
+public:
+
+
+
+    static IDatabase &getInstance()
+    {
+        static IDatabase instance;
+        return instance;
+    }
+
+    QString userLogin(QString userName,QString password);
+
+private:
+    explicit IDatabase(QObject *parent = nullptr);
+
+    IDatabase(IDatabase const &)=delete;
+    void operator=(IDatabase const & )=delete;
+
+    QSqlDatabase database;
+
+    void initDatabase();
+
+
+signals:
+
+public:
+    bool initPatientModel();
+    int addNewPatient();
+    bool searchPatient(QString filter);
+    bool deleteCurrentPatient();
+    bool submitPatientEdit();
+    void revertPatientEdit();
+
+    QSqlTableModel *patientTabModel;
+    QItemSelectionModel *thePatientSelection;
+
+
+};
+
+#endif // IDATABASE_H
