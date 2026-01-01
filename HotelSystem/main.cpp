04bcc0c32e4d001db124f5b56f3e1dee49ddc536
Floraluke <kiby1025@qq.com>
2026-01-01 14:27:34 +0800

    改完 main.cpp 的样式，使得界面更加美观

--- HotelSystem/main.cpp
+++ HotelSystem/main.cpp
@@ -14,6 +14,48 @@ int main(int argc, char *argv[])
     }
 
     MainWindow w;
+
+    // 设置全局样式表
+    QString qss = R"(
+    QMainWindow {
+        background-color: #f0f2f5;
+    }
+    QTableView {
+        border: 1px solid #dcdfe6;
+        background-color: white;
+        selection-background-color: #409eff;
+        gridline-color: #e4e7ed;
+    }
+    QHeaderView::section {
+        background-color: #f5f7fa;
+        padding: 5px;
+        border: none;
+        border-bottom: 1px solid #dcdfe6;
+        font-weight: bold;
+    }
+    QPushButton {
+        background-color: #409eff;
+        color: white;
+        border-radius: 4px;
+        padding: 6px 16px;
+        font-size: 14px;
+    }
+    QPushButton:hover {
+        background-color: #66b1ff;
+    }
+    QPushButton:pressed {
+        background-color: #3a8ee6;
+    }
+    /* 针对退房按钮做个红色警示 */
+    QPushButton#btnCheckOut {
+        background-color: #f56c6c;
+    }
+    QPushButton#btnCheckOut:hover {
+        background-color: #f78989;
+    }
+)";
+    a.setStyleSheet(qss);
+
     w.show();
     return a.exec();
 }
